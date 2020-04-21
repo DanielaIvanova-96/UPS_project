8adec6bac2b563f534cee3fd220bd5273e677afa
HristoF <hristo.fotakiev@gmail.com>
2020-04-21 20:33:39 +0300

    Added Projection Model

---
+++ cinema_usp/src/main/java/model/Projection.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Projection {
+    private int id_projection;
+    private int id_movie;
+
+    public int getId_projection() {
+        return id_projection;
+    }
+
+    public void setId_projection(int id_projection) {
+        this.id_projection = id_projection;
+    }
+
+    public int getId_movie() {
+        return id_movie;
+    }
+
+    public void setId_movie(int id_movie) {
+        this.id_movie = id_movie;
+    }
+}
